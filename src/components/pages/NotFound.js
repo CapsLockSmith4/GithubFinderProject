1dde7378eb13b8d0f189905378c89deea2e4f525
David Summers <djs327@newcastle.edu.au>
2021-02-18 09:54:34 +1100

    project finished ..but not ready for build

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>NOT FOUND</h1>
+            <p className="lead"> This is not the page your looking for...</p>
+        </div>
+    )
+}
+
+export default NotFound
